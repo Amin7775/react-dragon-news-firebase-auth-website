534c4d1eecd425628abf2285c10acebdcca675c7
Amin7775 <amin.dpi24@gmail.com>
2024-04-10 15:32:04 +0600

    Homepage Layout And Date Time Formatting Using Moment.Js

--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -1,8 +1,15 @@
+import moment from 'moment';
+import logo from './../../../assets/logo.png'
 
 const Header = () => {
+
+    const time = moment().format('dddd, MMMM Do YYYY');
+    
     return (
-        <div>
-            header
+        <div className='flex flex-col items-center py-5'>
+            <img className='mt-4' src={logo} alt="" />
+            <p className='mt-6 text-[#706F6F] text-lg'>Journalism Without Fear or Favour</p>
+            <p className='mt-3 text-[#706F6F]'>{time}</p>
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,27 @@
 import Header from "../../components/Shared/Header/Header";
+import LeftSideNav from "../../components/Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../../components/Shared/Navbar/Navbar";
+import RightSideNav from "../../components/Shared/RightSideNav/RightSideNav";
 
 const Home = () => {
     return (
-        <div className="text-2xl">
+        <div className="container mx-auto">
             <Header></Header>
             <Navbar></Navbar>
             this is home
+
+            {/* container */}
+            <div className="grid grid-cols-4 gap-6">
+                <div>
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="col-span-2">
+                    news coming soon
+                </div>
+                <div>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
